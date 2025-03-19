f3f4cdb62784d82d05461f4587e4332af23fa892
will-developer <william_limaa@hotmail.com>
2025-03-19 15:36:35 -0300

    feat: apply initial database migration

---
+++ backend/prisma/migrations/20250319182953_table_tecnologies/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Technology" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Tecnologia_pkey" PRIMARY KEY ("id")
+);
