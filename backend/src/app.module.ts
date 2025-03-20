483027c012e89dbcd68027cd659554aaae757154
will-developer <william_limaa@hotmail.com>
2025-03-20 16:26:32 -0300

    feat: generate projects module and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DbModule } from './db/db.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, ProjectsModule],
   controllers: [AppController],
 })
 export class AppModule {}
