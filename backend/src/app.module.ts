4e33e578ffad4f4d84f7f63d1fb22738b0cf773e
will-developer <william_limaa@hotmail.com>
2025-03-20 17:35:03 -0300

    feat: receive the database and perpetuate it through the controller and the provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import { DbModule } from './db/db.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TechnologyModule } from './technology/technology.module';
 import { PrismaProvider } from './db/prisma.provider';
+import { TechnologyProvider } from './technology/technology.provider';
 
 @Module({
   imports: [DbModule, ProjectsModule, TechnologyModule],
   controllers: [AppController],
-  providers: [PrismaProvider],
+  providers: [],
 })
 export class AppModule {}
