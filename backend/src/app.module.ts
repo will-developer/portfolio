2e8ca06e7a1b5c6656962fe82d286075347750ac
will-developer <william_limaa@hotmail.com>
2025-03-20 16:34:30 -0300

    fix: remove unnecessary db controller and add Prisma provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { DbModule } from './db/db.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TechnologyModule } from './technology/technology.module';
+import { PrismaProvider } from './db/prisma.provider';
 
 @Module({
   imports: [DbModule, ProjectsModule, TechnologyModule],
   controllers: [AppController],
+  providers: [PrismaProvider],
 })
 export class AppModule {}
--- backend/src/db/db.module.ts
+++ backend/src/db/db.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { DbController } from './db.controller';
+import { PrismaProvider } from './prisma.provider';
 
 @Module({
-  controllers: [DbController]
+  controllers: [PrismaProvider],
+  exports: [PrismaProvider],
 })
 export class DbModule {}
