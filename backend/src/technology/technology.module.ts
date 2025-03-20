175a9a068620d7f487a2bbe21e3c18525ef8c9c2
will-developer <william_limaa@hotmail.com>
2025-03-20 17:38:34 -0300

    fix: resolve import and command bugs in package.json

--- backend/src/technology/technology.module.ts
+++ backend/src/technology/technology.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TechnologyController } from './technology.controller';
+import { DbModule } from '../db/db.module';
+import { TechnologyProvider } from './technology.provider';
 
 @Module({
-  controllers: [TechnologyController]
+  imports: [DbModule],
+  controllers: [TechnologyController],
+  providers: [TechnologyProvider],
 })
 export class TechnologyModule {}
