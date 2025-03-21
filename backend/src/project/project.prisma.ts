f7ec03457886e5cebfc4ea7d8d658bc1d2dd4bce
will-developer <william_limaa@hotmail.com>
2025-03-21 09:22:33 -0300

    feat: add getAll method to project.prisma

--- backend/src/project/project.prisma.ts
+++ backend/src/project/project.prisma.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaProvider } from '../db/prisma.provider';
+import { Projects } from '../../../core/src';
 
 @Injectable()
-export class ProjectPrisma {}
+export class ProjectPrisma {
+  constructor(private readonly prisma: PrismaProvider) {}
+
+  async getAll(): Promise<Projects[]> {
+    return this.prisma.project.findMany() as any;
+  }
+}
