9996f0bf9d18b931d759fd9391c9baf63b723976
will-developer <william_limaa@hotmail.com>
2025-03-21 09:13:25 -0300

    feat: add DbModule to project route

--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProjectPrisma } from './project.prisma';
 import { ProjectController } from './project.controller';
+import { DbModule } from '../db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [ProjectController],
   providers: [ProjectPrisma],
 })
