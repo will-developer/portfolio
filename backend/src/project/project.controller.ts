c78193dda9d5cc14114ccacd3bd33ffa45edf19d
will-developer <william_limaa@hotmail.com>
2025-03-21 10:24:39 -0300

    feat: insert methods to projects controller

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProjectPrisma } from './project.prisma';
+import { Project } from '@prisma/client';
 
-@Controller('project')
-export class ProjectController {}
+@Controller('projects')
+export class ProjectController {
+  constructor(private readonly repository: ProjectPrisma) {}
+
+  @Get()
+  async getAll(): Promise<Project[]> {
+    return this.repository.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string): Promise<Project | null> {
+    return this.repository.getById(Number(id));
+  }
+}
